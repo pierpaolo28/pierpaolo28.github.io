aa11030ae76205c47f3eeeb9bcd7865af061b0b5
Pier Paolo Ippolito <40637715+pierpaolo28@users.noreply.github.com>
2019-04-20 15:54:00 +0100

    Update sent.php

--- sent.php
+++ sent.php
@@ -3,7 +3,7 @@
 This first bit sets the email address that you want the form to be submitted to.
 You will need to change this value to a valid email address that you can access.
 */
-$webmaster_email = "pier.py@hotmail.it";
+$webmaster_email = "emailtoreceivemessage@todo.com";
 
 /*
 This bit sets the URLs of the supporting pages.
