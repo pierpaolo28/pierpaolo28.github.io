309afff91416e8b86f96104028532f40d540bae9
Pier Paolo <ppi1u16@soton.ac.uk>
2021-09-09 02:42:20 +0100

    Progressive Web App test

--- sw.js
+++ sw.js
@@ -1,47 +1,45 @@
-let cacheName = "Pier Paolo Ippolito"; // 👈 any unique name
-let filesToCache = [ // Add URL you want to cache in this list.
-  // '/', // If you have separate JS/CSS files,
-  // './index.html', // add path to those files here
-  // '/assets/css/main.css',
-  // '/assets/img/favicon.jpg',
-  // '/assets/img/icons/android-chrome-192x192.png',
-  // '/assets/img/icons/android-chrome-256x256.png'
+// Credits to: https://gist.github.com/kosamari/7c5d1e8449b2fbc97d372675f16b566e
+var APP_PREFIX = 'PierPaoloIppolitoWebsite_' // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01' // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [ // Add URL you want to cache in this list.
+  './', // If you have separate JS/CSS files,
+  './index.html', // add path to those files here
+  './assets/css/main.css'
 ]
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(caches.open(cacheName).then((cache) => {
-    console.log('installed successfully')
-    return cache.addAll(filesToCache);
-  }));
-});
+// Respond with cached resources
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      return request || fetch(e.request)
+    })
+  )
+})
 
-self.addEventListener('fetch', function(event) {
-
-  if (event.request.url.includes('clean-cache')) {
-    caches.delete(cacheName);
-    console.log('Cache cleared')
-  }
-
-  event.respondWith(caches.match(event.request).then(function(response) {
-    if (response) {
-      console.log('served form cache')
-    } else {
-      console.log('Not serving from cache ', event.request.url)
-    }
-    return response || fetch(event.request);
-  }));
-});
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('service worker: Removing old cache', key);
-          return caches.delete(key);
+      var cacheWhitelist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function(key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i])
+          return caches.delete(keyList[i])
         }
-      }));
+      }))
     })
-  );
-  return self.clients.claim();
-});
+  )
+})
